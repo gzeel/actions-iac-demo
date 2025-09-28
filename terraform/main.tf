8a317a44b59c09049315bf65ee4bcc508e9a9d5f
Gerrit Scherpenzeel <g.scherpenzeel@windesheim.nl>
2025-09-28 13:26:57 +0200

    Initial Commit

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+provider "github" {
+  owner = var.repo_owner
+}
+
+# Label dat bij de eerste run wordt aangemaakt (idempotent)
+resource "github_issue_label" "deployed" {
+  repository = var.repo_name
+  name = "deployed"
+  color = "0e8a16"
+  description = "Created by Terraform"
+}
+
+# Een duidelijke, tastbare actie: een issue aanmaken bij succesvolle pipeline
+resource "github_issue" "deployment_success" {
+  repository = var.repo_name
+  title = "✅ Deployment successful"
+  body = "Created by Terraform at ${timestamp()}"
+  labels = [github_issue_label.deployed.name]
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "repo_owner" {
+  type = string
+  description = "Owner (org of user) van de GitHub repo"
+}
+
+variable "repo_name" {
+  type = string
+  description = "Naam van de GitHub repo"
+}
