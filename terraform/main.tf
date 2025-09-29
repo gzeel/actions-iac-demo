99ff678631741de24a6b6b8cd7a0d44d12cc96c4
Gerrit Scherpenzeel <g.scherpenzeel@windesheim.nl>
2025-09-29 11:03:56 +0200

    changed main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,10 +4,10 @@ provider "github" {
 
 # Label dat bij de eerste run wordt aangemaakt (idempotent)
 resource "github_issue_label" "deployed" {
-  repository  = var.repo_name
-  name        = "deployed"
-  color       = "0e8a16"
-  description = "Created by Terraform"
+repository  = var.repo_name
+name        = "deployed"
+color       = "0e8a16"
+description = "Created by Terraform"
 }
 
 # Een duidelijke, tastbare actie: een issue aanmaken bij succesvolle pipeline
